7fe9b1fa4993a8a686d6c58162a395895cc0a782
Ivan2344 <Ivan2344>
2024-04-29 08:44:07 +0200

    fixed ulam

--- SWEDStuff1/src/Ulamlol/ulammain.java
+++ SWEDStuff1/src/Ulamlol/ulammain.java
@@ -4,22 +4,29 @@ public class ulammain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(ulam(127));
+		System.out.println(ulam(999999));
 	}
 	
 	public static int ulam(int n) 
 	{
-		while(n > 1) 
-		{
-			if(n % 2 == 0) 
+		if (n <= 1000000)
+				{
+			while(n > 1) 
 			{
-				n = n/2;
-			}else 
-			{
-				n = (n*3) + 1;
+				if(n % 2 == 0) 
+				{
+					n = n/2;
+				}else 
+				{
+					n = (n*3) + 1;
+				}
 			}
+			return n;
+		}else 
+		{
+			return n;
 		}
-		return n;
+		
 	}
 
 }
